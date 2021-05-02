1093cda98b4bc65caf4b094f48bae59cac9bdfec
blhsrwznrghfzpr <blhsrwznrghfzpr.hompyhrln@gmail.com>
2021-05-02 21:24:55 +0900

    [ParanoiaRebooted] Resultを使用

--- lib/bcdice/game_system/ParanoiaRebooted.rb
+++ lib/bcdice/game_system/ParanoiaRebooted.rb
@@ -29,14 +29,8 @@ module BCDice
       register_prefix('ND', 'MP')
 
       def eval_game_system_specific_command(command)
-        case command
-        when /^ND/i
-          return get_node_dice_roll(command)
-        when /^MP/i
-          return get_mutant_power_roll(command)
-        else
-          return nil
-        end
+        get_node_dice_roll(command) ||
+          get_mutant_power_roll(command)
       end
 
       private
@@ -44,7 +38,7 @@ module BCDice
       def generate_roll_results(dices)
         computer_dice_message = ''
         results = dices.dup
-        if results[-1].to_i == 6
+        if results[-1] == 6
           results[-1] = 'C'
           computer_dice_message = '(Computer)'
         end
@@ -55,9 +49,9 @@ module BCDice
       def get_node_dice_roll(command)
         debug("eval_game_system_specific_command Begin")
 
-        m = /^ND((-)?\d+)/i.match(command)
+        m = /^ND(-?\d+)$/i.match(command)
         unless m
-          return ''
+          return nil
         end
 
         debug("command", command)
@@ -81,20 +75,21 @@ module BCDice
       def get_mutant_power_roll(command)
         debug("eval_game_system_specific_command Begin")
 
-        m = /^MP(\d+)/i.match(command)
+        m = /^MP(\d+)$/i.match(command)
         unless m
-          return ''
+          return nil
         end
 
         debug("command", command)
 
         parameter_num = m[1].to_i
-        dice_count = parameter_num.abs + 1
+        dice_count = parameter_num + 1
 
         dices = @randomizer.roll_barabara(dice_count, 6)
 
         failure_rate = dices.count(1)
-        message = failure_rate == 0 ? '成功' : "失敗(#{failure_rate})"
+        condition = failure_rate == 0
+        message = condition ? "成功" : "失敗(#{failure_rate})"
 
         results, computer_dice_message = generate_roll_results(dices)
 
@@ -102,7 +97,12 @@ module BCDice
 
         debug("eval_game_system_specific_command result")
 
-        return "(#{command}) ＞ [#{results.join(', ')}] ＞ #{message}#{computer_dice_message}"
+        text = "(#{command}) ＞ [#{results.join(', ')}] ＞ #{message}#{computer_dice_message}"
+
+        Result.new.tap do |r|
+          r.text = text
+          r.condition = condition
+        end
       end
     end
   end
